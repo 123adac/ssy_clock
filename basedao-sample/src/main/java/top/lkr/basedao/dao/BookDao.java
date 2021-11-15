2140c318bead08d35c6649bf3c940d40b3fab354
123adac <2269557372@qq.com>
2021-11-15 09:07:40 +0800

    Day01-基于泛型和反射封装&Maven项目的资源路径

---
+++ basedao-sample/src/main/java/top/lkr/basedao/dao/BookDao.java
@@ -0,0 +1,4 @@
+package top.lkr.basedao.dao;
+
+public interface BookDao<T> extends BaseDao<T> {
+}
---
+++ basedao-sample/src/main/java/top/lkr/basedao/dao/impl/BookDaoImpl.java
@@ -0,0 +1,7 @@
+package top.lkr.basedao.dao.impl;
+
+import top.lkr.basedao.dao.BookDao;
+import top.lkr.basedao.entity.Book;
+
+public class BookDaoImpl extends BaseDaoImpl<Book> implements BookDao<Book> {
+}
